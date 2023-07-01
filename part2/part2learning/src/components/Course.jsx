0ef06a562b28ba15c0f76a9736d921c67afb144f
Jericho Bermas <jecho@aslai.com>
2023-07-02 01:04:41 +0800

    Exercise 2.5 done.

--- part2/part2learning/src/components/Course.jsx
+++ part2/part2learning/src/components/Course.jsx
@@ -1,15 +1,22 @@
 const Course = ({ course }) => {
-  const parts = course.parts;
-  const total = parts.reduce((s, p) => s + p.exercises, 0);
   return (
     <div>
-      <h1>{course.name}</h1>
-      {parts.map((part, index) => (
-        <p key={part.id + "-" + index}>
-          {part.name} {part.exercises}
-        </p>
-      ))}
-      <b key="total">Total of {total} exercises</b>
+      {course.map((c) => {
+        const parts = c.parts;
+        const total = parts.reduce((s, p) => s + p.exercises, 0);
+
+        return (
+          <div key={c.id}>
+            <h1>{c.name}</h1>
+            {parts.map((part) => (
+              <p key={part.id}>
+                {part.name} {part.exercises}
+              </p>
+            ))}
+            <b>Total of {total} exercises</b>
+          </div>
+        );
+      })}
     </div>
   );
 };
